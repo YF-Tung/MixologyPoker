705badca30a2c12586dd32769dc5f19e5d3b1160
Yu-Fan Tung <yufantung@gmail.com>
2018-12-17 13:20:43 +0800

    Add next_thing_to_buy.py

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # MixologyPoker
-Recipe on Mixology poker
+Recipe of Mixology poker. Help you decide next thing to buy!
 
 Ingredients of each cocktail is listed in `poker.csv`.
 
 A full list of all the ingredients is listed in `ingredient.txt`.
+
+### Requirement
+`python 2.x / 3.x`
+
+### Guide
+1. Add/remove all the ingredients you have in `bar.txt`
+2. `python next_thing_to_buy.py`
+
+### Sample output
+```
+You can make 5 cocktail(s) currently.
+You can make 2 more by buying Lillet_blanc
+You can make 1 more by buying Ginger_ale
+You can make 1 more by buying Coffee_liqueur
+You can make 1 more by buying Amaretto
+You can make 1 more by buying Midori
+You can make 1 more by buying Cranberry
+You can make 1 more by buying Blue_curacao
+You can make 1 more by buying Soda
+You can make 1 more by buying Drambuie
+You can make 1 more by buying Tonic
+```
+
+### Acknowledgement
+All the recipes are from [Mixology](https://mixology.com.tw/). Kudos to them for popularizing mixology fashion in Taiwan.
---
+++ next_thing_to_buy.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import csv
+import copy
+import operator
+
+def get_bar(filename='bar.txt'):
+    """ Return a set containing what you have """
+    fin = open(filename)
+    ls = [x.strip() for x in list(fin.readlines()) if not x.startswith('#')]
+    return [x for x in ls if x]
+
+def get_recipes(filename='poker.csv'):
+    """ Return a list of (name, [ingredients]) tuple """
+    with open(filename) as csvfile:
+        recipes = csv.reader(csvfile)
+        return [(x[0],x[1:]) for x in list(recipes)]
+
+def main():
+    recipes = get_recipes()
+    bar = get_bar()
+    lack_list = {}
+    for _, recipe in recipes:
+        lack = [x for x in recipe if x not in bar]
+        num_lack = len(lack)
+        if num_lack not in lack_list:
+            lack_list[num_lack] = []
+        lack_list[num_lack].append(lack)
+
+    # Recipes that you can make already
+    print ('You can make {} cocktail(s) currently.'.format(len(lack_list.get(0, []))))
+
+    # Recipes that you can make by buying one
+    if 1 in lack_list:
+        count = {}
+        for x in lack_list[1]:
+            if x[0] in count:
+                count[x[0]] += 1
+            else:
+                count[x[0]] = 1
+        sorted_count = sorted(count.items(), key=operator.itemgetter(1))
+        for tpl in reversed(sorted_count):
+            print ('You can make {} more by buying {}'.format(
+                tpl[1], tpl[0]))
+    else:
+        print ('You cannot make more cocktails by just buying another ingredient.')
+
+if __name__ == '__main__':
+    main()
